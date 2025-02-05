ca595a9f780d5983f57df858c3ce8b39b25e083f
DevalsinhCollab <deval@collabsoftech.com.au>
2025-02-05 15:25:43 +0530

    db changes

--- DB/db.js
+++ DB/db.js
@@ -6,7 +6,7 @@ env.config();
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`connected : ${conn.connection.host}`);
+    console.log(`mongodb connected : ${conn.connection.host}`);
 
   } catch (error) {
     console.log(error.message);
